99096b67ecf7c5fbe7508028043077fa50440cf9
@lessfoobar <59063885+lessfoobar@users.noreply.github.com>
2025-07-10 16:34:35 +0000

    as

--- postcss.config.js
+++ postcss.config.js
@@ -2,26 +2,16 @@ module.exports = {
   plugins: [
     // Process @import statements first
     require('postcss-import')({
-      path: ['css']
+      path: ['css', 'css/base', 'css/components', 'css/layout', 'css/utilities']
     }),
     
     // Add vendor prefixes
-    require('autoprefixer'),
-    
-    // Minify CSS for production (only if NODE_ENV is production)
-    ...(process.env.NODE_ENV === 'production' ? [
-      require('cssnano')({
-        preset: ['default', {
-          discardComments: {
-            removeAll: true,
-          },
-          normalizeWhitespace: true,
-          discardDuplicates: true,
-          mergeRules: true,
-          colormin: true,
-          minifySelectors: true,
-        }]
-      })
-    ] : [])
+    require('autoprefixer')({
+      overrideBrowserslist: [
+        '> 1%',
+        'last 2 versions',
+        'not dead'
+      ]
+    })
   ]
 };
