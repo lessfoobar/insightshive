5b5fa19b3ca63086e57957c8e52e128bd68dde25
@lessfoobar <59063885+lessfoobar@users.noreply.github.com>
2025-07-21 10:14:10 +0000

    rqwafds

--- js/modules/mobile-menu.js
+++ js/modules/mobile-menu.js
@@ -138,8 +138,8 @@ export class MobileMenu {
 
   handleOutsideClick = (e) => {
     // Only close if clicking outside both menu toggle and nav links
-    if (this.isOpen && 
-        !this.menuToggle.contains(e.target) && 
+    if (this.isOpen &&
+        !this.menuToggle.contains(e.target) &&
         !this.navLinks.contains(e.target)) {
       this.closeMenu();
     }
